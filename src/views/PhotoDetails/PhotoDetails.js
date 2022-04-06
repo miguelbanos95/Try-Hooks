a221878b33f86d38611bd2fabc4d4d98bb9a9692
Miguel <info.miguelbanos@gmail.com>
2022-04-06 10:30:52 +0200

    Details fixed

--- src/views/PhotoDetails/PhotoDetails.js
+++ src/views/PhotoDetails/PhotoDetails.js
@@ -4,21 +4,36 @@ import { getPhoto } from '../../service/PhotosService';
 
 
 const PhotoDetails = () => {
-    const [photo, setPhoto] = useState([])
+    const [photo, setPhoto] = useState(null)
     const [loading, setLoading] = useState(true)
-    const { id } = useParams(); 
+    const { id } = useParams();
 
-    useEffect(() =>{
-        getPhoto(id).then(photo =>
-            setPhoto(photo))
-            setLoading(false)
+    useEffect(() => {
+        getPhoto(id).then(photo => setPhoto(photo));
+        setLoading(false);
     }, [])
+    console.log(photo)
 
     return (
         <div>
-            <img src={photo.thumbnailUrl} alt="" />
-            <h1>{photo.title}</h1>
-        </div>
+            {
+                loading ? (
+                    <div className='d-flex justify-content-center align-items-center' style={{ height: 150 }}>
+                        <div className="spinner-border text-success" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                ) : (photo &&
+                    <>
+                        <h1 className='fs-1 mt-2'>{photo.data.title.toUpperCase()}</h1>
+                        <div className='mt-4'>
+                            <img src={photo.data.url} style={{ width: 300, height: 300 }} alt=""/>
+                            <img src={photo.data.thumbnailUrl} className='ms-2' alt="" />
+                        </div >
+                    </>
+                )
+            }
+        </div >
     );
 };
 
